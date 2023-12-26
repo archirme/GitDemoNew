341a2f21fed241027bd04bdaf1203677eaee0f7b
Anil C <achirme@gmail.com>
2023-12-26 17:33:31 +0530

    First Commit

---
+++ src/main/java/SeleniumTesting/E2EProjectNew/App.java
@@ -0,0 +1,13 @@
+package SeleniumTesting.E2EProjectNew;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
